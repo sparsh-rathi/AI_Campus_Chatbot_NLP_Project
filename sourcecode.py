146e7e344b091664beb50d68a4b6934dae2f4b15
sparsh-rathi <93386308+sparsh-rathi@users.noreply.github.com>
2023-12-11 18:14:22 +0530

    Updated sourcecode.py with a GUI interface

--- sourcecode.py
+++ sourcecode.py
@@ -1,162 +1,113 @@
-import nltk
-nltk.download('wordnet')
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import pickle
-import numpy as np
-from tensorflow.keras.models import load_model
-model = load_model('chatbot_model.h5')
-import json
-import random
 from pprint import pprint
-intents = json.loads(open('nlp.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
+import random
+import json
+from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+from nltk.stem import WordNetLemmatizer
+import nltk
+from tkinter import Tk, Scrollbar, Text, Entry, Button, END, Label
+from tkinter import ttk
+
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('database.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(
+        word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
+    bag = [0]*len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                # assign 1 if current word is in the vocabulary position
+        for i, w in enumerate(words):
+            if w == s:
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return (np.array(bag))
+
 
 def predict_class(sentence, model):
-    
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    ERROR_THRESHOLD = 0.0
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-        
     return return_list
-
-def getResponse(sen, ints, intents_json):
-    tag = ints[0]['intent']
-    result=""
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            if (i['tag'] == 'greeting' or i['tag'] == 'goodbye' or i['tag'] == 'thanks'  or i['tag'] == 'noanswer' or i['tag'] == 'options' or i['tag'] == 'students_info'):
-                result = random.choice(i['responses'])
-                break
-            elif(i['tag']== 'get_student_name'):
-                lis = list(sen.split(' '))
-                l= len(lis)
-                j= lis[l-1]
-                k="".join(j)
-                sn=i['responses']
-                for a in sn:
-                    for b in a['students']:
-                        if(b.lower() == k.lower()):
-                            result =  a['students'][b]
-                            break
-                        else:
-                            continue
-                if(result==""):
-                    result="name for given roll number not found"
-            elif(i['tag']== 'get_student_roll'):
-                lis = list(sen.split(' '))
-                for idx,val in enumerate(lis):
-                    if(val =='of'):
-                        n=idx
-                        break
-                j= lis[n+1:]
-                k="".join(j)
-                sid=i['responses']
-                for a in sid:
-                    for b in a['students']:
-                        if((a['students'][b].replace(' ', '').lower())==(k.replace(' ', '').lower())):
-                            result = b
-                            break
-                        else:
-                            continue
-                if(result==""):
-                    result=" roll number for given student name not found"
-                    
-            elif(i['tag'] == 'second_floor'):
-                lis = list(sen.split(' '))
-                print(lis)
-                print()
-                l= len(lis)
-                for idx,val in enumerate(lis):
-                    if(val =='is'):
-                        n=idx
-                        break
-                j= lis[n+1:]
-                k="".join(j)
-                print(k)
-                print()
-                sn=i['responses']
-                for a in sn:
-                    for b in a['directions']:
-                        if(b.replace(' ', '').lower() == k.replace(' ', '').lower()):
-                            result =  a['directions'][b]
-                            break
-                        else:
-                            continue
-                if(result==""):
-                    result="Location not found"
-
-            elif(i['tag'] == 'get_labexam_date'):
-                lis = list(sen.split(' '))
-                l=len(lis)
-                for idx,val in enumerate(lis):
-                    if(val == 'of'):
-                        n = idx
-                        break
-                j = lis[n+1:]
-                k="".join(j)
-                sid=i['responses']
-                for a in sid:
-                    for b in a['dates']:
-                        if((b.replace(' ','').lower()) == (k.replace(' ','').lower())):
-                            result = a['dates'][b]
-                            break
-                        else:
-                            continue
-                    if(result==""):
-                        result=" please enter a valid subject"
-    return result
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
+
+    if not ints:
+        print("No intent predicted.")
+        return "I'm sorry, I didn't understand that."
+
+    tag = ints[0]['intent']
     res = getResponse(msg, ints, intents)
     return res
 
-def chat():
-    print("Chatbot CLI - Type 'exit' to end the conversation.")
-    while True:
-        user_input = input("You: ")
+
+class ChatGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Campus Bots")
         
-        if user_input.lower() == 'exit':
-            print("Goodbye!")
-            break
+        master.configure(bg='#d5bdaf')
 
-        if user_input != '':
-            print("You: " + user_input)
+        self.chat_history = Text(
+            master,
+            wrap="word",
+            state="disabled",
+            height=20,
+            width=50,
+            bg='#E3D5CA', 
+            relief="flat", 
+            font=("Calibri", 12), 
+        )
+        self.chat_history.pack(expand=True, fill="both", padx=10, pady=10)  
 
-            response = chatbot_response(user_input)
-            print("Bot:", response)
+        self.input_entry = Entry(master, width=50)
+        self.input_entry.bind("<Return>", self.send_message)
+        self.input_entry.pack(side="left", expand=True, fill="x", padx=10, pady=10)  
+
+        send_button = Button(master, text="Send", command=self.send_message)
+        send_button.pack(side="right", padx=10, pady=10)  
+
+    def send_message(self, event=None):
+        user_input = self.input_entry.get()
+        self.input_entry.delete(0, END)
+
+        if user_input.lower() == "exit":
+            self.master.destroy()
+            return
+
+        response = chatbot_response(user_input)
+        self.update_chat_history(f"You: {user_input}\nBot: {response}\n")
+
+    def update_chat_history(self, message):
+        self.chat_history.config(state="normal")
+        self.chat_history.insert("end", message)
+        self.chat_history.config(state="disabled")
+        self.chat_history.see("end")
+
 
 if __name__ == "__main__":
-    chat()
+    root = Tk()
+    root.geometry("600x500+200+100")
+    style = ttk.Style(root)
+    style.configure("TButton", padding=6, relief="flat", background="#4CAF50", foreground="#FFFFFF") 
+
+    gui = ChatGUI(root)
+    root.mainloop()
